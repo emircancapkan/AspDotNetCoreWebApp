8cc8991ac6e59e99074d9cbdcb19863d02246de3
emircancapkan <emircancapkan@stu.khas.edu.tr>
2024-10-17 17:10:19 +0300

    .

--- WebApplication3/WebApplication3.Web/Controllers/ProductsController.cs
+++ WebApplication3/WebApplication3.Web/Controllers/ProductsController.cs
@@ -74,9 +74,9 @@ namespace WebApplication3.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(Product updateProduct){
+        public IActionResult Update(Product updateProduct, int productId){
 
-
+            updateProduct.Id = productId;
             _context.Products.Update(updateProduct);
             _context.SaveChanges();
             TempData["status"]="The Product is updated succesfully!";
